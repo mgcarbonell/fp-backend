c2ff1dfeab2f330ec9dd9b95435a12343741817c
mgcarbonell <mgcarbonell@gmail.com>
2020-12-10 10:19:47 -0800

    Update branch

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_socketio import SocketIO, send
+from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///countzero' #dotenv will need to go here.
 app.config['SECRET_KEY'] = 'placeholder' #dotenv will need to go here.
-socketio = SocketIO(app)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 class User(db.Model):
   __tablename__ = 'users'
@@ -16,6 +16,7 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String, unique=True, nullable=False)
   username = db.Column(db.String, nullable=False)
+  password = db.Column(db.String, nullable=False)
   bio = db.Column(db.String(175))
 
   def __repr__(self):
