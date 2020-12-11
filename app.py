ca03d748bb6ecd3b3b141800dde756539c17df2b
mgcarbonell <mgcarbonell@gmail.com>
2020-12-10 16:38:19 -0800

    Restructure away from api.py and models.py to app.py, database.py, models.py

---
+++ app.py
@@ -0,0 +1,34 @@
+
+from flask import Flask, render_template_string
+from flask_security import Security, current_user, auth_required, hash_password, SQLAlchemySessionUserDatastore
+from database import db_session, init_db
+from models import User, Role
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Generate a nice key using secrets.token_urlsafe()
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+# Bcrypt is set as default SECURITY_PASSWORD_HASH
+# Generate a good salt using: secrets.SystemRandom().getrandbits(128)
+app.config['SECURITY_PASSWORD_SALT'] = os.environ.get("SECURITY_PASSWORD_SALT")
+
+# Setup Flask-Security
+user_datastore = SQLAlchemySessionUserDatastore(db_session, User, Role)
+security = Security(app, user_datastore)
+
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    init_db()
+    user_datastore.create_user(email="test@me.com", password=hash_password("password"))
+    db_session.commit()
+
+# Views
+@app.route("/")
+@auth_required()
+def home():
+    return render_template_string('Hello {{email}} !', email=current_user.email)
+
+if __name__ == '__main__':
+    app.run()
--- models.py
+++ models.py
@@ -1,21 +1,67 @@
-from flask import Flask
+import os
+
+from database import Base
+from flask import Flask, render_template_string
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from flask_security import Security, SQLAlchemySessionUserDatastore, auth_required, hash_password
+from flask_security.models import fsqla_v2 as fsqla
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///countzero' #dotenv will need to go here.
-app.config['SECRET_KEY'] = 'placeholder' #dotenv will need to go here.
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+fsqla.FsModels.set_db_info(db)
 
-class User(db.Model):
+
+roles_user = db.Table('roles_users',
+  db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+  db.Column('role_id', db.Integer, db.ForeignKey(role.id)))
+
+
+friendship = db.Table('friendships',
+  db.Column('friend_a_id', db.Integer, db.ForeignKey('user.id'),
+                                          primary_key=True),
+  db.Column('friend_b_id', db.Integer, db.ForeignKey('user.id'),
+                                          primary_key=True))
+
+class User(db.Model, fsqla.FsUserMixin):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String, unique=True, nullable=False)
   name = db.Column(db.String, nullable=False)
-  password = db.Column(db.String, nullable=False)
+  password = db.Column(db.String(255), nullable=False)
   bio = db.Column(db.String(150))
+  active = db.Column(db.Boolean)
+  confirmed_at = db.Column(db.DateTime)
+  roles = db.relationship('Roles', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+  friends = relationship('User', secondary=friendship,
+                          primaryjoin=id==friendship.c.friend_a_id,
+                          secondaryjoin=id==friendship.c.friend_b_id,
+                          backref=db.backref('users', lazy='dynamic'))
 
   def __repr__(self):
       return f'User(id={self.id}, email="{self.email}", name="{self.name}")'
+
+friendship_union = select([
+                            friendship.c.friend_a_id,
+                            friendship.c.friend_b_id
+                            ]).union(
+                                select([
+                                        friendship.c.friend_b_id,
+                                        friendship.c.friend_a_id]
+                                )
+                                ).alias()
+User.all_friends = relationship('User', 
+                          secondary=friendship_union,
+                          primaryjoin=User.id==friendship_union.c.friend_a_id,
+                          secondaryjoin=User.id==friendship_union.c.friend_b_id,
+                          viewonly=True)
+
+class Role(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(40))
+  description: db.Column(db.String(255))
+
+user_datastore = SQLAlchemySessionUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
